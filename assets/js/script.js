5c8caa428bd3b8b58f1b0fb8e07a359711d5a892
Lindan Maulana <lindanstemga@gmail.com>
2025-04-17 20:10:36 +0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,3 +190,73 @@ const switchIconMode = () => {
     }
   }
 };
+
+const renderCodeHtml = () => {
+  const codeHtml = document.getElementById("code-html")
+
+  codeHtml.innerHTML = `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Todolist</title>
+    <link rel="stylesheet" href="./assets/css/style.css" />
+  </head>
+  <body>
+    <header>
+      <h1 class="header__name">Dika Ramdhanika</h1>
+      <button onclick="handleMode()" class="btn__mode">
+        <img
+          id="icon__mode"
+          src="./assets/images/night.svg"
+          alt="night-mode"
+          width="40"
+          height="32"
+        /> 
+      </button>
+    </header>
+    <main> 
+      <section class="todo">
+        <div class="container">
+          <h2 class="title">To-Do-List</h2>
+          <div class="todo__content">
+            <form onsubmit="handleSubmit(event)" class="todo__content__form">
+              <input
+                type="text"
+                placeholder="Tambah catatan baru..."
+                class="todo__content__form__input"
+                name="todo"
+              />
+              <button type="submit" class="todo__content__form__action">
+                Add
+              </button>
+            </form>
+            <div class="todo__content__action">
+              <button onclick="handleModeTask(1)" class="todo__content__action__all mode__all modeActive">All</button>
+              <button onclick="handleModeTask(2)" class="todo__content__action__pending mode__pending">Pending</button>
+              <button onclick="handleModeTask(3)" class="todo__content__action__complete mode__complete">Complete</button>
+            </div>
+            <ul class="list__content" id="list-todo">
+              <li class="list__content__item notfound">
+                <p class="list__content__item__text text__notfound">Catatan kosong</p>
+                <img
+                class="list__content__item__notfound"
+                  src="./assets/images/xmark.svg"
+                  alt="xmark"
+                  width="25"
+                  height="25"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </main>
+    <script type="text/javascript" src="./assets/js/script.js"></script>
+  </body>
+</html>
+  `
+}
+
+renderCodeHtml()
